fb1a0b5fd7be64c483aa98f1d6113e2beab3aea0
Mariyan Karadzhov <mariyan.karadzhov@mail.bg>
2016-03-25 16:39:07 +0200

    Създаване на програмата Здрасти БРАТ!

---
+++ ZdrastiBRAT/ZdrastiBRAT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ZdrastiBRAT
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Zdrasti BRATO!!!");
+			Console.ReadKey ();
+		}
+	}
+}
